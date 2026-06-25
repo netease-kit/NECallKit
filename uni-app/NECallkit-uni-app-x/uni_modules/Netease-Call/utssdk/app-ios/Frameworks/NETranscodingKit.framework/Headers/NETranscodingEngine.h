e375ee76a00c3bae1d264247673427c3e693ce30
hzyuan <hzyuan@corp.netease.com>
2026-06-25 16:17:03 +0800

    uniapp-x 更新4.7.5 uts 插件

---
+++ uni-app/NECallkit-uni-app-x/uni_modules/Netease-Call/utssdk/app-ios/Frameworks/NETranscodingKit.framework/Headers/NETranscodingEngine.h
@@ -0,0 +1,14 @@
+//// Copyright (c) 2022 NetEase, Inc. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+#import <Foundation/Foundation.h>
+#import <NERtcCallUIKit/NERtcCallUIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NETranscodingEngine : NSObject <NETranscodingDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
